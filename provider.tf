c7f55a06249d21f04f0a898099e29fefbc6a99c5
Deligent18 <129704440+Deligent18@users.noreply.github.com>
2024-12-04 14:11:45 +0200

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,15 +1,5 @@
-{
-    "version": "0.2.0",
-    "configurations": [
-        {
-            "name": "Connect to dlv server",
-            "type": "go",
-            "debugAdapter": "dlv-dap",
-            "request": "attach",
-            "mode": "remote",
-            "remotePath": "${workspaceFolder}",
-            "port": 2345,
-            "host": "127.0.0.1",
-        }
-    ]
+provider "aws" {
+  region     = "us-east-1"
+  access_key = "AKIAU72LF5VT3P73GJVH"
+  secret_key = "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"
 }
